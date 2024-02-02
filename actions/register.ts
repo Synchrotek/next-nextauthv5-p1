ac4c3cad536bd2fcfec2cab0607a38840de0c814
Satyajit Nayak <satyajitnayak348@gmail.com>
2024-02-02 06:55:44 +0530

    user creation =1

--- actions/register.ts
+++ actions/register.ts
@@ -1,6 +1,8 @@
 "use server";
+
 import * as z from "zod";
-import { revalidatePath, revalidateTag } from "next/cache";
+import bcrypt from 'bcrypt';
+import { db } from '@/lib/db';
 import { RegisterSchema } from "@/schemas/inedx";
 
 export const registerAction = async (values: z.infer<typeof RegisterSchema>) => {
@@ -10,5 +12,25 @@ export const registerAction = async (values: z.infer<typeof RegisterSchema>) => 
         return { error: "Invalild fields!" };
     }
 
-    return { success: "Email sent1" };
+    const { email, password, name } = validatedFields.data;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const existingUser = await db.user.findUnique({
+        where: { email, }
+    });
+    if (existingUser) {
+        return { error: "Email already in use!" };
+    }
+
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword,
+        }
+    });
+
+    // TODO : Send verification email %%%%%%%%%%%%%%%%%%%%%%%%
+
+    return { success: "User created!" };
 }
